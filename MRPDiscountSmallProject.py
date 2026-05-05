fb6baa9dc8ffdc7db2ea21e09d4e8606952e6d64
Yashasvi Dalvi <yashasvidalvi3@gmail.com>
2026-05-05 05:39:11 +0530

    Add MRP discount calculator

--- MRPDiscountSmallProject.py
+++ MRPDiscountSmallProject.py
@@ -1,10 +1,10 @@
-mrp = float(input("Enter the MRP of the product: "))
+mrp = eval(input("Enter the MRP of the product: "))
 
-discount = float(input("Enter the discount percentage: "))
+discount = eval(input("Enter the discount percentage: "))
 
 selling_price = mrp - (mrp * discount / 100)
 
-print("\n--- Selling Price Calculation ---")
-print(f"MRP: Rs. {mrp}")
-print(f"Discount: {discount}%")
-print(f"Selling Price: Rs. {selling_price}")
+print("--- Selling Price Calculation ---")
+print("MRP: Rs. " + str(mrp))
+print("Discount: " + str(discount) + "%")
+print("Selling Price: Rs. " + str(selling_price))
