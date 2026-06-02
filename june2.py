43f389f26c3d3538e0733286c481bcc5e90a87e7
Yashasvi Dalvi <yashasvidalvi3@gmail.com>
2026-06-02 05:55:09 +0530

    june2.py

---
+++ june2.py
@@ -0,0 +1,50 @@
+# t = (10,)
+# print(type(t))
+
+# a = 10
+# print(a)
+# a = 20.5
+# # print(a)
+
+# t = (1,2,3)
+# print(id(t))
+# print(t)
+# t =(1,2,3,4)
+# print(id(t))
+# print(t)
+
+# i = []
+# print(id(i))
+# i.append(10)
+# i.append(20.5)
+# print(id(i))
+# print(i)
+
+# c1 = "T shirt"
+# c2 = "Cash"
+# c3 = "charger"
+# bag = c1,c2,c3
+# print(type(bag))
+
+# bag1 = ("T shirt","Cash","charger")
+# bag2 = ["T shirt","Cash","charger"]
+
+# a, *b, c = bag1
+# print(a)
+# print(b)
+# print(c)
+
+#swapping
+num1=10
+num2=20
+
+# num3 =num1
+# num1 = num2
+# num2=num3
+
+# print(num1)
+# print(num2)
+
+num1, num2 = num2, num1
+print(num1)
+print(num2)
