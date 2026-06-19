26dfc10d70e296348b5ab6d51790f482980d5963
Yashasvi Dalvi <yashasvidalvi3@gmail.com>
2026-06-19 06:15:05 +0530

    HW

--- june19.py
+++ june19.py
@@ -1,5 +1,6 @@
 # a = [12,34,6,8,9]
 
+#sum , average
 # total = 0
 # count = 0
 # for i in a:
@@ -18,6 +19,7 @@ students = {
     "hanuman":"130"
 }
 
+#sum , average
 # total = 0
 # count = 0
 
@@ -28,6 +30,8 @@ students = {
 # print(total)
 # print(average)
 
+
+#max marks
 # max_marks = 0
 # max_name = ""
 # for name, marks in students.items():
@@ -38,11 +42,15 @@ students = {
 # print(max_name)
 
 
+# input name and print marks
+
 # person = input("Enter name:")
 
 # for name, marks in students.items():
 #     if name == person:
 #         print(marks)
+
+#count of people of marks 130
 
 # count = 0 
 # for name in students:
@@ -50,6 +58,7 @@ students = {
 #         count += 1
 # print(count)
 
+#increase marks by 10
 # l2 ={}
 # for name in students:
 #     marks = int(students[name])
@@ -57,6 +66,8 @@ students = {
 #     l2[name] = str(marks)
 # print(l2)
 
+
+#reduce marks by 20 if found "am" in name
 l2 = {}
 for name in students.keys():
     if "am" in name:
